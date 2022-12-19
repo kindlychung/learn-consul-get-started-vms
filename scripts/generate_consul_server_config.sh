705290c8bfb47d8f46b740546ac8dbe40eabf317
keenz <kindlychung@gmail.com>
2022-12-19 02:21:06 +0100

    Update generate_consul_server_config.sh

--- scripts/generate_consul_server_config.sh
+++ scripts/generate_consul_server_config.sh
@@ -9,6 +9,8 @@ export DNS_RECURSOR=${DNS_RECURSOR:-"1.1.1.1"}
 export HTTPS_PORT=${HTTPS_PORT:-"8443"}
 export DNS_PORT=${DNS_PORT:-"8600"}
 
+export PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
 echo "Clean existing configuration"
 rm -rf ${CONSUL_DATA_DIR}/
 rm -rf ${CONSUL_CONFIG_DIR}/
@@ -18,15 +20,15 @@ mkdir -p ${CONSUL_CONFIG_DIR} && mkdir -p ${CONSUL_DATA_DIR}
 
 STAT=$?
 
-if [ ${STAT} -ne 0 ];  then
-  echo "Folder creation failed, exiting."
-  exit 1;
+if [ ${STAT} -ne 0 ]; then
+	echo "Folder creation failed, exiting."
+	exit 1
 fi
 
 cd ${CONSUL_CONFIG_DIR}
 
 echo "Generate agent configuration - agent-server-secure.hcl"
-tee ${CONSUL_CONFIG_DIR}/agent-server-secure.hcl > /dev/null << EOF
+tee ${CONSUL_CONFIG_DIR}/agent-server-secure.hcl >/dev/null <<EOF
 # agent-server-secure.hcl
 # Data Persistence
 data_dir = "${CONSUL_DATA_DIR}"
@@ -57,12 +59,13 @@ enable_local_script_checks = true
 EOF
 
 echo "Generate server configuration - agent-server-specific.hcl"
-tee ${CONSUL_CONFIG_DIR}/agent-server-specific.hcl > /dev/null << EOF
+tee ${CONSUL_CONFIG_DIR}/agent-server-specific.hcl >/dev/null <<EOF
 ## Server specific configuration for ${DATACENTER}
 server = true
 bootstrap_expect = 1
 datacenter = "${DATACENTER}"
-client_addr = "127.0.0.1"
+client_addr = "0.0.0.0"
+advertise_addr = "$PUBLIC_IP"
 ## UI configuration (1.9+)
 ui_config {
   enabled = true
@@ -70,51 +73,41 @@ ui_config {
 EOF
 
 echo "Generate gossip encryption key configuration - agent-gossip-encryption.hcl"
-echo encrypt = \"$(consul keygen)\" > ${CONSUL_CONFIG_DIR}/agent-gossip-encryption.hcl
+if [[ ! -f ${CONSUL_CONFIG_DIR}/agent-gossip-encryption.hcl ]]; then
+	echo encrypt = \"$(consul keygen)\" >${CONSUL_CONFIG_DIR}/agent-gossip-encryption.hcl
+fi
 
-echo "Create CA for Consul datacenter"
-consul tls ca create -domain=${DOMAIN}
-
-echo "Create server Certificate and key pair"
-consul tls cert create -server -domain ${DOMAIN} -dc=${DATACENTER}
+# echo "Create CA for Consul datacenter"
+# consul tls ca create -domain=${DOMAIN}
+# echo "Create server Certificate and key pair"
+# consul tls cert create -server -domain ${DOMAIN} -dc=${DATACENTER}
 
 echo "Generate TLS configuration - agent-server-tls.hcl"
-tee ${CONSUL_CONFIG_DIR}/agent-server-tls.hcl > /dev/null << EOF
+tee ${CONSUL_CONFIG_DIR}/agent-server-tls.hcl >/dev/null <<EOF
 # TLS Encryption (requires cert files to be present on the server nodes)
 tls {
   defaults {
-    ca_file   = "/etc/consul/config/consul-ca.pem"
-    cert_file = "/etc/consul/config/server.pem"
-    key_file  = "/etc/consul/config/server-key.pem"
-    verify_outgoing        = true
-    verify_incoming        = true
+    ca_file   = "${CONSUL_CONFIG_DIR}/consul-ca.pem"
+    cert_file = "${CONSUL_CONFIG_DIR}/server.pem"
+    key_file  = "${CONSUL_CONFIG_DIR}/server-key.pem"
+    verify_outgoing        = false
+    verify_incoming        = false
   }
   https {
     verify_incoming        = false
   }
   internal_rpc {
-    verify_server_hostname = true
+    verify_server_hostname = false
   }
 }
-### TLS Encryption (requires cert files to be present on the server nodes)
-#ca_file   = "/etc/consul/config/consul-agent-ca.pem"
-#cert_file = "${CONSUL_CONFIG_DIR}/${DATACENTER}-server-${DOMAIN}-0.pem"
-#key_file  = "${CONSUL_CONFIG_DIR}/${DATACENTER}-server-${DOMAIN}-0-key.pem"
-#verify_incoming        = false
-#verify_incoming_rpc    = true
-#verify_outgoing        = true
-#verify_server_hostname = true
-#auto_encrypt {
-#  allow_tls = true
-#}
 EOF
 
 echo "Generate ACL configuration - agent-server-acl.hcl"
-tee ${CONSUL_CONFIG_DIR}/agent-server-acl.hcl > /dev/null << EOF
+tee ${CONSUL_CONFIG_DIR}/agent-server-acl.hcl >/dev/null <<EOF
 ## ACL configuration
 acl = {
   enabled = true
-  default_policy = "deny"
+  default_policy = "allow"
   enable_token_persistence = true
   enable_token_replication = true
   down_policy = "extend-cache"
@@ -126,7 +119,7 @@ consul validate ${CONSUL_CONFIG_DIR}
 
 STAT=$?
 
-if [ ${STAT} -ne 0 ];  then
-  echo "Configuration invalid. Exiting."
-  exit 1;
+if [ ${STAT} -ne 0 ]; then
+	echo "Configuration invalid. Exiting."
+	exit 1
 fi
